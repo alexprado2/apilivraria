7bbd5c0e171537ddd785d9d1105be523b9aa53b7
DEVAlexHenrique <alex.henrique1212@gmail.com>
2024-12-17 12:51:58 -0300

    Add project files.

---
+++ Controllers/LivrariaBaseController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLivraria.Controllers;
+[Route("[controller]")]
+[ApiController]
+public class LivrariaBaseController : ControllerBase
+{
+}
---
+++ Controllers/LivrosController.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLivraria.Controllers;
+public class LivrosController : LivrariaBaseController
+{
+}
